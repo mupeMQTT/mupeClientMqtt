b849f7113401001e9dca858c168b2140292f2e50
mupeMQTT <mqtt@bluewin.ch>
2023-02-25 07:46:50 +0100

    MQTT Client

--- mupeClientMqtt.c
+++ mupeClientMqtt.c
@@ -32,18 +32,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
 	switch ((esp_mqtt_event_id_t) event_id) {
 	case MQTT_EVENT_CONNECTED:
 		ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-		msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1,
-				0);
-		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
 
-		msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
-		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-		msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
-		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
-
-		msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
-		ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
 		break;
 	case MQTT_EVENT_DISCONNECTED:
 		ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
@@ -51,9 +40,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
 
 	case MQTT_EVENT_SUBSCRIBED:
 		ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-		msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0,
-				0);
-		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
 		break;
 	case MQTT_EVENT_UNSUBSCRIBED:
 		ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
@@ -77,7 +64,6 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
 					event->error_handle->esp_transport_sock_errno);
 			ESP_LOGI(TAG, "Last errno string (%s)",
 					strerror(event->error_handle->esp_transport_sock_errno));
-
 		}
 		break;
 	default:
